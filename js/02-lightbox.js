1c1572ec037570baeddee727c1872c10e251569a
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-13 00:04:42 +0200

    Gallery with SlightLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  let listItem = document.createElement("li");
+  gallery.append(listItem);
+  listItem.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`
+  );
+});
+
+gallery.addEventListener("click", clickImage);
+
+function clickImage(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery li a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
