7a5bcf0a01a7ab1dd029da0e50da5decc489c239
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-12 21:52:11 +0200

    Gallery with module

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  let listItem = document.createElement("li");
+  gallery.append(listItem);
+  listItem.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+gallery.addEventListener("click", imageClick);
+
+function imageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  gallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
